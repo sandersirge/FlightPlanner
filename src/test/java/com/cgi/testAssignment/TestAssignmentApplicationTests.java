5065e6a124438b92e7638479be62650a39c362a3
sandersirge <sandersirge@gmail.com>
2025-03-27 17:22:19 +0200

    Project initialized

---
+++ src/test/java/com/cgi/testAssignment/TestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.testAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
